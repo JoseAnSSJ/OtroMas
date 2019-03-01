f357dcebcfb13c5a4e540d9b198cbf353b8c76e6
JoseAntonio <joseantonio.garcia.rdz@gmail.com>
2019-03-01 16:56:20 -0600

    Validacion

--- app/src/main/java/com/example/pablo/prueba7/Modelos/GetDeepMODORDSERModel.java
+++ app/src/main/java/com/example/pablo/prueba7/Modelos/GetDeepMODORDSERModel.java
@@ -1,17 +1,18 @@
 package com.example.pablo.prueba7.Modelos;
 
 public class GetDeepMODORDSERModel {
-    private int BaseIdUser;
+	private int BaseIdUser;
 
-    public GetDeepMODORDSERModel(int baseIdUser) {
-        BaseIdUser = baseIdUser;
-    }
+	public GetDeepMODORDSERModel(int baseIdUser) {
+		BaseIdUser = baseIdUser;
+	}
 
-    public int getBaseIdUser() {
-        return BaseIdUser;
-    }
+	public int getBaseIdUser() {
+		return BaseIdUser;
+	}
 
-    public void setBaseIdUser(int baseIdUser) {
-        BaseIdUser = baseIdUser;
-    }
+	public void setBaseIdUser(int baseIdUser) {
+		BaseIdUser = baseIdUser;
+	}
 }
+
