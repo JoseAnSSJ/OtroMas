339da6f7f4710821a199bd0a3d67164db57950b5
JoseAntonio <joseantonio.garcia.rdz@gmail.com>
2019-03-26 15:18:41 -0600

    new request

--- app/src/main/java/com/example/pablo/prueba7/Services/Services.java
+++ app/src/main/java/com/example/pablo/prueba7/Services/Services.java
@@ -1976,6 +1976,41 @@ public class Services {
         token = Util.getTokenPreference(Util.preferences);
         return token;
     }
+    
+    public Service RequestPost(final Context context, JSONObject jsonObject) {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        final RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+
+        final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+
+            @Override
+            public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+                //Modificacion del Header
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", getToken(context))
+                        .addHeader("Content-Type", "application/json")
+                        .post(body)
+                        .build();
+
+
+                return chain.proceed(newRequest);
+            }
+        }).build();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.NEW_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(Service.class);
+
+    }
+    //-----
+    public void RequestGet(){
+
+    }
 }
 
 
