e0f99241b55c82f03e5a3d86b762c389ed099181
brayan reyes <brayanhuy19@gmail.com>
2019-04-08 17:44:18 -0500

    ok

--- app/src/main/java/com/example/pablo/prueba7/Fragments/EjecutarFragment.java
+++ app/src/main/java/com/example/pablo/prueba7/Fragments/EjecutarFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.pablo.prueba7.Activitys.Inicio;
 import com.example.pablo.prueba7.Dibujo.Firma;
 import com.example.pablo.prueba7.Listas.Array;
 import com.example.pablo.prueba7.Modelos.DeepConsModel;
@@ -43,7 +44,7 @@ public class EjecutarFragment extends Fragment {
     private Request request = new Request();
     public static ProgressDialog dialogEjecutar;
     Inicio in;
-    Button firmar;
+    Button firma;
     Button salir;
 
     public EjecutarFragment() {
@@ -64,7 +65,7 @@ public class EjecutarFragment extends Fragment {
         eject = view.findViewById(R.id.ejec);
         msgEjecutarOrd = view.findViewById(R.id.msgEjecutarOrd);
         ejecutar = view.findViewById(R.id.ejecutarLay);
-        firmar = view.findViewById(R.id.firmarOrd);
+        firma = view.findViewById(R.id.firmarOrd);
         reiniciar.setEnabled(false);
         salir = view.findViewById(R.id.salirEjecutarOrd);
 
